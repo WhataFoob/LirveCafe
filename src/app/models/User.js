2189d1c6011f3bec358093dbbb795ac13ea49c58
JJLee-20194099 <JJLee-20194099>
2021-10-17 12:11:35 +0700

    add User Model

---
+++ src/app/models/User.js
@@ -0,0 +1,30 @@
+import mongoose, { mongo } from 'mongoose';
+import slug from 'mongoose-slug-generator';
+import mongooseDelete from 'mongoose-delete';
+import mongooseEmail from 'mongoose-type-email';
+
+const Schema = mongoose.Schema;
+
+const User = new Schema(
+    {
+        firstname: {type: String, required: true},
+        lastname: {type: String, required: true},
+        email: {type: mongoose.SchemaTypes.Email, required: true},
+        phone: {type: String, required: true},
+        username: {type: String},
+        password: {type: String, required: true},
+        avatar: {type: String},
+        hidden: {type: Boolean}
+    },
+    {
+        timestamps: true,
+    }
+);
+
+mongoose.plugin(slug);
+User.plugin(mongooseDelete, {
+    deleteAt: true,
+    overrideMethods: 'all'
+})
+
+export default mongoose.model('User', User);
