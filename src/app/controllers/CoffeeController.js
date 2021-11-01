35986d259b2c60c7111c4eb72c4c6de4c903ee18
JJLee-20194099 <JJLee-20194099>
2021-11-01 12:22:56 +0700

    Handling comment in Coffee Controller.js

--- src/app/controllers/CoffeeController.js
+++ src/app/controllers/CoffeeController.js
@@ -1,4 +1,7 @@
 import Coffee from '../models/Coffee.js';
+import Comment from '../models/Comment.js';
+import Reply from '../models/Reply.js';
+
 import { 
     singleMongooseDocumentToObject,
     mongooseDocumentsToObject
@@ -18,11 +21,18 @@ const CoffeeController = {
 
     // GET /coffee/:slug
     show(req, res, next) {
+        
         Coffee.findOne({slug: req.params.slug})
             .then((coffee) => {
-                res.render('drink/item/coffee_info.hbs', {
-                    coffee: singleMongooseDocumentToObject(coffee)
-                })
+                coffee = singleMongooseDocumentToObject(coffee)
+                Comment.find({itemId: coffee._id})
+                    .then((commentList) => {
+                        res.render('drink/item/coffee_info.hbs', {
+                            coffee: coffee,
+                            commentList: mongooseDocumentsToObject(commentList)
+                        })
+                    })
+               
             }).catch(next);
     },
 
@@ -79,6 +89,39 @@ const CoffeeController = {
         Coffee.restore({_id: req.params.id})
         .then(() => res.redirect('back'))
         .catch(next);
+    },
+
+    // POST /coffee/do-comment
+
+    doComment(req, res, next) {
+       
+        const comment = new Comment(req.body);
+        comment.save()
+            .then(() => res.send("Comment Successfully !!!"))
+            .catch(next)
+    },
+
+    // POST /coffee/reply-comment
+
+    replyComment(req, res, next) {
+        const reply = new Reply(req.body);
+        console.log(req.body)
+        
+        Comment.findOne({_id: req.body.parentCommentId})
+            .then((comment) => {
+                const replyList = comment.replyList
+                replyList.push(reply);
+                comment.replyList = replyList;
+                return new Promise(function(resolve) {
+                    comment.save(() => resolve())
+                })
+            })
+            .then(function() {
+                return new Promise(function(resolve) {
+                    reply.save(() => resolve())
+                })
+            })
+            .catch(next)
     }
 }
 
