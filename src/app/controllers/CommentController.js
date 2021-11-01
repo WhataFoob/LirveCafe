95b31aafd121b32ecb7aa1216c3c26818584cb1a
JJLee-20194099 <JJLee-20194099>
2021-11-01 17:21:53 +0700

    Add Comment Controller

---
+++ src/app/controllers/CommentController.js
@@ -0,0 +1,49 @@
+import Comment from '../models/Comment.js';
+import Reply from '../models/Reply.js';
+
+import { 
+    singleMongooseDocumentToObject,
+    mongooseDocumentsToObject
+} from '../../support_lib/mongoose.js';
+
+const CommentControllers = {
+    
+    // POST /do-comment
+
+     doComment(req, res, next) {
+       
+        const comment = new Comment(req.body);
+        comment.save()
+            .then(() => res.send(singleMongooseDocumentToObject(comment)))
+            .catch(next)
+    },
+
+    // POST /reply-comment
+
+    replyComment(req, res, next) {
+        const reply = new Reply(req.body);
+
+        Comment.findOne({_id: req.body.parentCommentId})
+            .then((comment) => {
+                const replyList = comment.replyList
+                replyList.push(reply);
+                comment.replyList = replyList;
+                return new Promise(function(resolve) {
+                    comment.save()
+                    resolve()
+                })
+            })
+            .then(function() {
+                return new Promise(function(resolve) {
+                    reply.save()
+                    resolve()
+                })
+            })
+            .then(() => {
+                res.send("Reply comment Successfully")
+            })
+            .catch(next)
+    }
+}
+
+export default CommentControllers;
