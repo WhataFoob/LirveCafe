494a0b7208fac32bb026e6f5a827f47d98cae5ae
JJLee-20194099 <JJLee-20194099>
2021-11-21 15:49:15 +0700

    Update front end pages

--- src/index.js
+++ src/index.js
@@ -68,13 +68,31 @@ io.on("connection", function(socket) {
         socket.join(data.itemId)
         socket.join(data._id)
         console.log(socket.adapter.rooms)
-        io.sockets.in(data.itemId).emit("server_send_comment_to_coffee_item", data)
+        // If not want to bother users
+        // io.sockets.in(data.itemId).emit("server_send_comment_to_coffee_item", data)
+
+        // whatever
+        io.sockets.emit("server_send_comment_to_coffee_item", data)
     })
+
+    socket.on("client_send_comment_to_book_item", function(data) {
+        socket.join(data.itemId)
+        socket.join(data._id)
+        console.log(socket.adapter.rooms)
+        // If not want to bother users
+        // io.sockets.in(data.itemId).emit("server_send_comment_to_book_item", data)
+
+        // whatever
+        io.sockets.emit("server_send_comment_to_book_item", data)
+
+    })
+
     
     socket.on("client_send_reply_comment", function(data) {
         socket.join(data.parentCommentId)
         console.log(data.parentCommentId)
         console.log(socket.adapter.rooms)
+        console.log("haha")
         io.sockets.in(data.parentCommentId).emit("server_send_reply_comment", data)
     })
 
