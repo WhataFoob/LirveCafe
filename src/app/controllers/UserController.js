c8cbbc8454f9f008e52a4d8f79edd56b62d9ae29
WhataFoob <93764963+WhataFoob@users.noreply.github.com>
2021-12-05 16:41:36 +0700

    edit phần update
    chỉnh lại updateone() để cập nhật thông tin, in thay đổi ra terminal

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,7 +16,6 @@ const UserController = {
                 res.render('own/users/list/store.hbs', {
                     users: mongooseDocumentsToObject(users),
                     user: res.locals.user
-                   
                 });
             }).catch(next);
     },
@@ -25,9 +24,8 @@ const UserController = {
     show(req, res, next) {
         User.findOne({ _id: req.params.id })
             .then((user) => {
-                res.render('users/item/user_info.hbs', {
-                    user: singleMongooseDocumentToObject(user),
-                    user: res.locals.user
+                res.render('users/info/item/user_info.hbs', {
+                    user: singleMongooseDocumentToObject(user)
                 })
             })
             .catch(next);
@@ -57,9 +55,8 @@ const UserController = {
     edit(req, res, next) {
         User.findById(req.params.id)
             .then((user) => {
-                res.render('users/item/edit.hbs', {
-                    user: singleMongooseDocumentToObject(user),
-                    user: res.locals.user
+                res.render('users/info/item/edit.hbs', {
+                    user: singleMongooseDocumentToObject(user)
                 })
             })
             .catch(next);
@@ -67,8 +64,11 @@ const UserController = {
 
     // PATCH /users/:id
     update(req, res, next) {
-        User.updateOne({_id: req.params.id}, req.body)
-            .then(() => res.redirect('back'))
+        User.updateOne({_id: req.params.id}, {firstname:req.body.firstname, lastname:req.body.lastname, email: req.body.email,phone:req.body.phone, avatar: req.body.avatar})
+            .then(() => {
+                console.log(req.body)
+                res.redirect('back')
+            })
             .catch(next);
     },
 
